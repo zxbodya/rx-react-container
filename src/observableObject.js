a3d96051bb49648a72ddb4403b2d59c79507a0f7
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-04-22 18:48:19 +0300

    fix observableObject  - it should not emit complete

--- src/observableObject.js
+++ src/observableObject.js
@@ -2,12 +2,12 @@
 
 let Rx = require('rx');
 
-function objectObserver(observables) {
+function observableObject(observables) {
   var keys = Object.keys(observables);
   var valueObservables = keys.map(key=>observables[key]);
 
   if (keys.length === 0) {
-    return Rx.Observable.return({});
+    return Rx.Observable.never().startWith({});
   }
 
   return Rx.Observable.combineLatest(valueObservables, (...values)=> {
@@ -18,4 +18,5 @@ function objectObserver(observables) {
     return res;
   })
 }
-module.exports = objectObserver;
+
+module.exports = observableObject;
