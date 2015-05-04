d7d7b39709c58b903f67bf989f5b59a6ab9f5cac
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-05-04 23:39:56 +0300

    Enchance RxComponent to prevent rerendering of whore virtual dom instead of one component, added internal HOC RxProxy to host RxCompoent

--- src/RxComponent.js
+++ src/RxComponent.js
@@ -6,12 +6,43 @@ const {AnonymousObservable} = Rx;
 
 const objectObserver = require('./observableObject');
 
+function createProxyComponent(Component, observable, initialState) {
+  class RxProxy extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+    }
+
+    componentWillMount() {
+      this.setState(initialState);
+    }
+
+    componentDidMount() {
+      this.subscribtion = observable.subscribe((state)=> {
+        this.setState(state);
+      });
+    }
+
+    componentWillUnmount() {
+      this.subscribtion.dispose();
+    }
+
+    render() {
+      return (<Component {...this.props} {...this.state}/>);
+    }
+  }
+  return RxProxy;
+}
+
+/**
+ * Creates observable form ready to render ReactElements.
+ * The same ReactElement would be emitted on every observables combination.
+ */
 class RxComponent extends AnonymousObservable {
   /**
    * @param {React.Component} Component
-   * @param {Object.<Rx.Observable>} observables
-   * @param {Object.<Rx.Observer>}observers
-   * @param {Object} props
+   * @param {Object.<string, Rx.Observable>=} observables
+   * @param {Object.<string, Rx.Observer>=}observers
+   * @param {Object=} props
    */
   constructor(Component, observables = {}, observers = {}, props = {}) {
     super(observer=> {
@@ -21,20 +52,28 @@ class RxComponent extends AnonymousObservable {
         callbacks[key] = (value)=>observers[key].onNext(value);
       });
 
-      return objectObserver(observables).map((state)=> {
-        return (
-          <Component {...props} {...state} {...callbacks}/>
-        );
-      }).subscribe(observer);
+      const propsObservable = objectObserver(observables).share();
+
+      const initialState = {};
+      const Proxy = createProxyComponent(Component, propsObservable, initialState);
+      const proxyElement = (
+        <Proxy {...props} {...callbacks}/>
+      );
+
+      return propsObservable
+        .do(state=>Object.assign(initialState, state))
+        .map(()=>proxyElement)
+        .subscribe(observer);
     });
+
     this.params = [Component, observables, observers, props];
   }
 
   /**
    * Extend defined params
-   * @param {Object.<Rx.Observable>} observables
-   * @param {Object.<Rx.Observer>}observers
-   * @param {Object} props
+   * @param {Object.<string, Rx.Observable>=} observables
+   * @param {Object.<string, Rx.Observer>=} observers
+   * @param {Object=} props
    * @returns {RxComponent}
    */
   extend(observables, observers, props) {
