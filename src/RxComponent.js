e4159d346f79cb49b17c7f43d6529c26d15a195d
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-05-21 01:10:34 +0300

    change RxComponent signature: in order to preserve context now it returns ReactComponent instead of ReactElement

--- src/RxComponent.js
+++ src/RxComponent.js
@@ -52,13 +52,11 @@ class RxComponent extends AnonymousObservable {
 
       const initialState = {};
       const Proxy = createProxyComponent(Component, propsObservable, initialState);
-      const proxyElement = (
-        <Proxy {...props} {...callbacks}/>
-      );
+      Proxy.defaultProps = Object.assign({}, props, callbacks);
 
       return propsObservable
         .do(state=>Object.assign(initialState, state))
-        .map(()=>proxyElement)
+        .map(()=>Proxy)
         .subscribe(observer);
     });
 
@@ -84,7 +82,7 @@ class RxComponent extends AnonymousObservable {
 
   /**
    * Extend defined params
-   * @param {function} decorator
+   * @param {function(component: React.Component): React.Component} decorator
    * @returns {RxComponent}
    */
   decorate(decorator) {
