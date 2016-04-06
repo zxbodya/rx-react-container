d237019c1cca5307b388348455d070a6e6c2d990
Bogdan Savluk <savluk.bogdan@gmail.com>
2016-04-06 16:08:54 +0300

    add missing firefox-launcher, bump version

--- karma.conf.js
+++ karma.conf.js
@@ -94,9 +94,10 @@ module.exports = function (config) {
 
     plugins: [
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine'),
       require('karma-sourcemap-loader'),
       require('karma-webpack'),
     ],
   });
-}
+};
